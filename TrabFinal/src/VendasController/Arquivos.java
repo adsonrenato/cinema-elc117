8a92ea90acaf4499a3902d978785539e62df37a9
gmiollo <gmiollo@inf.ufsm.br>
2013-07-23 12:43:08 +0000

    arquivos oka ?

---
+++ TrabFinal/src/VendasController/Arquivos.java
@@ -0,0 +1,61 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package VendasController;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+/**
+ *
+ * @author Guilherme
+ */
+public class Arquivos {
+     File arquivo = new File( "relatorio.csv" );
+     
+     public void criaarquivo(){
+       try {  
+              
+            boolean success = arquivo.createNewFile();  
+            if (success) {  
+                // arquivo n existia e foi criado
+            } else {  
+                // arquivo ja existia
+             }  
+        } catch (IOException e) { 
+            e.printStackTrace();
+        }  
+     }
+     
+     public void inserenomescolunas(){
+         try{
+            FileWriter fw = new FileWriter(arquivo, false);
+            BufferedWriter bw = new BufferedWriter(fw);
+            bw.write("Filme, Sala, Data, Horario, Ingressos Vendidos");
+            
+            
+         }catch(IOException e){
+             e.printStackTrace();
+         }
+       
+         
+     }
+     
+     public void insere(String filme, String sala, String data, String horario,int ingressos){
+       try{
+            
+            FileWriter fw = new FileWriter( arquivo, true );
+            BufferedWriter bw = new BufferedWriter( fw );
+            bw.write(filme+", "+sala+", "+data+", "+horario+", "+ingressos);
+            bw.newLine();
+            bw.close();
+            fw.close();
+       }catch(IOException e){
+            e.printStackTrace();
+       }
+    }
+    
+}
